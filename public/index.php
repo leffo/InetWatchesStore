543c812251d873a343077748d721e89ec2b91066
Alexandr Yakovlev <leffalt@gmail.com>
2020-11-07 12:16:37 +0700

    less6 - ready routing

--- public/index.php
+++ public/index.php
@@ -4,7 +4,10 @@ use ishop\App;
 
 require_once dirname(__DIR__) . '/config/init.php';
 require_once LIBS . '/functions.php';
+require_once CONF . '/routes.php';
 
 new App();
 
-throw new Exception('Страница не найдена', 404);
+// echo "Здравствуй, мир!<br>";
+// debug(\ishop\Router::getRoutes());
+// throw new Exception('Страница не найдена', 404);
---
+++ vendor/ishop/core/Router.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace ishop;
+
+
+class Router
+{
+    protected static $routes = [];
+    protected static $route = [];
+
+    public static function add($regexp, $route = [])
+    {
+        self::$routes[$regexp] = $route;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getRoutes()
+    {
+        return self::$routes;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getRoute()
+    {
+        return self::$route;
+    }
+
+
+    /**
+     * @param string $url
+     * @return void
+     */
+    public static function dispatch($url)
+    {
+        if (self::matchRoute($url)) {
+            echo "OK";
+        } else {
+            echo "NO";
+        }
+    }
+
+    /**
+     * @param string $url
+     * @return bool
+     */
+    public static function matchRoute($url)
+    {
+        return false;
+    }
+}
