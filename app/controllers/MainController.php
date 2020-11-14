680d308d4b7ceae90d5d0b9c6eafc42c8a1390ee
Alexandr Yakovlev <leffalt@gmail.com>
2020-11-14 15:42:22 +0700

    p2less4 - add cart of product

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -12,10 +12,11 @@ class MainController extends AppController
     public function indexAction()
     {
         $brands = R::find('brand', 'LIMIT 3');
+        $hits = R::find('product', "hit = '1' AND status = '1' LIMIT 8");
         debug($brands);
 
         //  Передача метаданных установкой.
         $this->setMeta(App::$app->getProperty('shop_name'), 'Описание...', 'Ключевые_слова...');
-        $this->set(compact('brands'));
+        $this->set(compact('brands', 'hits'));
     }
 }
