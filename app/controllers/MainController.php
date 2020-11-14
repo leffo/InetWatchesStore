ec608e00bb490b2ff55e7128ab6bcd09ee9de231
Alexandr Yakovlev <leffalt@gmail.com>
2020-11-14 14:25:18 +0700

    p2less3 - add brands ribbon output to site

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -5,13 +5,17 @@ namespace app\controllers;
 
 
 use ishop\App;
+use R;
 
 class MainController extends AppController
 {
     public function indexAction()
     {
+        $brands = R::find('brand', 'LIMIT 3');
+        debug($brands);
+
         //  Передача метаданных установкой.
         $this->setMeta(App::$app->getProperty('shop_name'), 'Описание...', 'Ключевые_слова...');
-
+        $this->set(compact('brands'));
     }
 }
