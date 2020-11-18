c6bc1b8cc1e938a8c07e7e98a54e2551f36fb4bb
leffo <leffalt@gmail.com>
2020-11-18 12:31:11 +0700

    p2less12 - product cart 0:0:40

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -9,7 +9,23 @@ class ProductController extends AppController
     public function viewAction()
     {
         $alias =  $this->route['alias'];
-        $product = \R::findOne('product',  "alias = ? AND STATUS = '1'", [$alias]);
-        debug($product);
+        $product = \R::findOne('product',  "alias = ? AND status = '1'", [$alias]);
+        if (!$product) {
+            throw new \Exception('Страница не найдена', 404);
+        }
+        // TODO хлебные крошки
+
+        // TODO связанные товары
+
+        // TODO запись в куку запрошенного товара
+
+        // TODO просмотренные товары
+
+        // TODO галерея
+
+        // TODO модификации товара
+
+        $this->setMeta($product->title, $product->description, $product->keywords);
+        $this->set(compact('product'));
     }
 }
--- app/views/layouts/watches.php
+++ app/views/layouts/watches.php
@@ -12,7 +12,9 @@ License URL: http://creativecommons.org/licenses/by/3.0/
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
     <link href="megamenu/css/ionicons.min.css" rel="stylesheet" type="text/css" media="all" />
     <link href="megamenu/css/style.css" rel="stylesheet" type="text/css" media="all" />
-     <!--Custom-Theme-files-->
+    <!-- FlexSlider -->
+    <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+    <!--Custom-Theme-files-->
     <!--theme-style-->
     <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
     <!--//theme-style-->
@@ -185,6 +187,41 @@ License URL: http://creativecommons.org/licenses/by/3.0/
     });
 </script>
 <script src="megamenu/js/megamenu.js"></script>
+<script src="js/imagezoom.js"></script>
+<script defer src="js/jquery.flexslider.js"></script>
+<script>
+    // Can also be used with $(document).ready()
+    $(window).load(function() {
+        $('.flexslider').flexslider({
+            animation: "slide",
+            controlNav: "thumbnails"
+        });
+    });
+</script>
+<!--dropdown-->
+<script src="js/jquery.easydropdown.js"></script>
+<script type="text/javascript">
+    $(function() {
+
+        var menu_ul = $('.menu_drop > li > ul'),
+            menu_a  = $('.menu_drop > li > a');
+
+        menu_ul.hide();
+
+        menu_a.click(function(e) {
+            e.preventDefault();
+            if(!$(this).hasClass('active')) {
+                menu_a.removeClass('active');
+                menu_ul.filter(':visible').slideUp('normal');
+                $(this).addClass('active').next().stop(true,true).slideDown('normal');
+            } else {
+                $(this).removeClass('active');
+                $(this).next().stop(true,true).slideUp('normal');
+            }
+        });
+
+    });
+</script>
 <script src="js/main.js"></script>
 <!--End-slider-script-->
 
