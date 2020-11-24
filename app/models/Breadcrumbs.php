f33eea19c047579607053065337c8d737dd563ec
leffo <leffalt@gmail.com>
2020-11-24 18:00:21 +0700

    p2less16 - breadcrumbs

---
+++ app/models/Breadcrumbs.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace app\models;
+
+
+class Breadcrumbs extends AppModel
+{
+    public static function getBreadcrumbs()
+    {
+        
+    }
+
+    public static function getParts()
+    {
+        
+    }
+}
